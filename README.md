0898b838d291ef74e8420094f177e03f7a76563c
Arash Kazemi <contact.arash.kazemi@gmail.com>
2023-07-18 06:18:31 +0330

    Initial commit.

---
+++ README.md
@@ -0,0 +1,66 @@
+## Dentity
+### A Lightweight Decoder and Encoder for HTML Entities
+
+Dentity is a small library for encoding and decoding HTML entities. It covers
+all the W3C defined entities, supports strict and non-strict conversions, and
+can work both in browser and node environments, which means that it doesn't use 
+DOMParser or similar methods. It is fast and also small. The minified version
+is only around 31KB, and smaller than 14KB when gzipped. It is also well tested.
+
+
+### Installation
+
+The latest source code of Dentity can be found at
+[https://github.com/arashkazemi/dentity](https://github.com/arashkazemi/dentity)
+
+To use in other node projects, install Dentity from npm public repository:
+
+        npm install dentity  
+
+To use in a webpage, download the source code and extract it. The minified 
+script itself is available in the `/dist` directory. 
+
+
+### Usage
+
+To encode a string, use the `encode` function:
+
+        Dentity.encode("hello > < ≠");
+
+which results in
+
+         "hello &gt; &lt; &ne;" 
+
+and similarly you can use `decode` function to do the reverse transform:
+
+        Dentity.decode("hello &gt; &lt; &ne;");
+
+which will give `"hello > < ≠"` back.
+
+The `decode` function takes a second argument `is_strict` which if true causes 
+the function to only accept W3C valid encodings and doesn't accept the exceptions 
+like `&copy` (without the semicolon at the end). Its default value is false.
+
+A helper function `registerPrototypes` is also available, that would register 
+both functions on String.prototype as `encodeHTML` and `decodeHTML` so the above 
+examples can be done like:
+
+        "hello > < ≠".encodeHTML()
+
+and 
+
+        "hello &gt; &lt; &ne;".decodeHTML()
+
+which can be very helpful in most situations, but this is not the default behavior
+because of the possibility of unwanted consequences like name collisions. So one
+should call 
+
+        Dentity.registerPrototypes();
+
+once after importing Dentity in order to use them.
+
+---
+
+Copyright (C) 2023 Arash Kazemi <contact.arash.kazemi@gmail.com>
+
+Dentity project is subject to the terms of BSD-2-Clause License. See the `LICENSE` file for more details.
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+/* 
+
+  Dentity - A Lightweight Decoder and Encoder for HTML Entities
+  Copyright (C) 2023 Arash Kazemi <contact.arash.kazemi@gmail.com>
+  
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+const path = require('path');
+const fs = require('fs');
+
+module.exports = {
+  entry: './src/dentity.js',
+  mode: 'production',
+  optimization: {
+    //minimize: false,
+  },
+  output: {
+    filename: 'dentity.min.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'Dentity',
+    libraryTarget: 'var'
+  },
+  plugins: [
+    {
+      apply: (compiler) => {
+        compiler.hooks.done.tap('license-plugin', (compilation) => {
+          fs.readFile('./dist/dentity.min.js', 'utf8', function(err, jssc) {
+            fs.readFile('./LICENSE', 'utf8', function(err, lsc) {
+              let out=`/*\n${lsc}\n*/\n\n${jssc}\n`;
+              fs.writeFile('./dist/dentity.min.js', out, {}, function(){}); 
+            });
+          });
+
+        });
+      }
+    }]
+};
